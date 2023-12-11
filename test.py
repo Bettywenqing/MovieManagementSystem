2449fb8ac2a01bba20c43c1ef13d33b688f2f91f
Bettywenqing <151025138+Bettywenqing@users.noreply.github.com>
2023-12-11 14:51:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import string,random
 
 def generateRandomString(length=10):
-    """生成随机10字节长度的字符串，为什么主键不用自增？？？？？？？？？？？？？？？？？？ 脱裤子放屁 脑残"""
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for i in range(length))
 
